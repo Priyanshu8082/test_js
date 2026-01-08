0bb0354c053803075f0c93fc94d4126a5e28501b
Priyanshu Chauhan <pc0749711189@gmail.com>
2026-01-09 00:05:14 +0530

    new upload

--- app.js
+++ app.js
@@ -4,12 +4,17 @@ let qrText=document.querySelector("#qrtext");
 function generateQr(){
    if(qrText.value.length>0){
       qrimg.src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+qrText.value;
+        let utterance = new SpeechSynthesisUtterance(qrText.value);
+        utterance.lang = "hi-IN";   // Hindi
+        utterance.rate = 1;         // normal speed
+        utterance.pitch = 1;        // normal pitch
+        speechSynthesis.speak(utterance);
    }
    else{
       setTimeout(()=>{
          qrText.classList.add("shake");
          qrText.style.border="3px solid red";
-      },300)
+      },100)
       setTimeout(() => {
         qrText.classList.remove("shake"); 
          qrText.style.border="2px solid #494eea";
@@ -18,6 +23,6 @@ function generateQr(){
         utterance.rate = 1;         // normal speed
         utterance.pitch = 1;        // normal pitch
         speechSynthesis.speak(utterance);
-      }, 1000);
+      }, 500);
    }
 }
